86ff910ca9db50aa5d21d7b3975f2bd788cc461a
javad_KH <166110182+Javadkh82@users.noreply.github.com>
2024-06-05 20:03:21 +0330

    Add files via upload

---
+++ chage.ino
@@ -0,0 +1,19 @@
+
+const int ledPin = 13;
+const int interrupt = 2;
+volatile bool led = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interrupt, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interrupt), change, CHANGE);
+}
+
+void loop() {
+  digitalWrite(ledPin, led);
+}
+
+void change() {
+  led = !led;
+}
+
